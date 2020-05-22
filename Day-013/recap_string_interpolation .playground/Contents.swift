3a0f54517547cce89cbdd6ca73825855f8757052
Welby Jennings <hello@zakemedia.com.au>
2020-05-22 14:50:18 +1000

    Day-013
    Consolidation Day 1
    - String interpolation

---
+++ Day-013/recap_string_interpolation .playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+// String interpolation
+
+var name = "Tim McGraw"
+"Your name is \(name)"
+
+var name2 = "Tim McGraw"
+"Your name is " + name2
+
+var name3 = "Tim McGraw"
+var age = 25
+var latitude = 36.166667
+
+"Your name is \(name3), your age is \(age), and your latitude is \(latitude)"
+
+var age2 = 25
+"You are \(age2) years old. In another \(age2) years you will be \(age2 * 2)."
