5eacc9e20e4959ba465998c7b5acff36e9921cb3
Welby Jennings <welby@zakemedia.com.au>
2020-05-22 14:24:17 +1000

    Day-013
    Consolidation Day 1
    - Operators

---
+++ Day-013/recap_operators.playground/Contents.swift
@@ -0,0 +1,54 @@
+import UIKit
+
+// Operators
+
+/*
+ Mdulus is written using a percent symbol: %.
+ It means “divide the left hand number evenly by the right, and return the remainder.”
+ 
+ So, 9 % 3 returns 0 because 3 divides evenly into 9, whereas 10 % 3 returns 1, because 10 divides by 3 three times, with remainder 1
+ */
+
+var a = 10
+a = a + 1
+a = a - 1
+a = a * a
+
+
+var b = 10
+b += 10
+b -= 10
+
+
+var a1 = 1.1
+var b1 = 2.2
+var c = a1 + b1
+
+var name1 = "Tim McGraw"
+var name2 = "Romeo"
+var both = name1 + " and " + name2
+
+// Comparison operators
+var a2 = 1.1
+var b2 = 2.2
+var c2 = a + b
+
+c2 > 3
+c2 >= 3
+c2 > 4
+c2 < 4
+
+
+var name3 = "Tim McGraw"
+name3 == "Tim McGraw"
+
+
+var stayOutTooLate = true
+stayOutTooLate
+!stayOutTooLate
+
+
+var name4 = "Tim McGraw"
+name4 == "Tim McGraw"
+name4 != "Tim McGraw"
+// You can also use ! with = to make != or "not equal"
