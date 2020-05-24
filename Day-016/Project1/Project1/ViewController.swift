bf55d3bfe235fdb6479296b4a5e24e47d572df30
Welby Jennings <hello@zakemedia.com.au>
2020-05-25 00:43:26 +1000

    Day-017
    Project 1 - Storm Viewer
    - Building a detail screen
    - Loading images with UIImage
    - Final tweaks
    - 2/3 Challenges

--- Day-016/Project1/Project1/ViewController.swift
+++ Day-016/Project1/Project1/ViewController.swift
@@ -14,26 +14,45 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true // forces large size title
+        
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path)
     
         for item in items {
             if item.hasPrefix("nssl") {
-                // the is a picture to load!
+                // this is a picture to load!
                 pictures.append(item)
             }
         }
         print(pictures)
     }
    
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return pictures.count
     }
     
+    // table view cells are reused when they are go off the sceeen
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
-        cell.textLabel?.text = pictures[indexPath.row]
+        cell.textLabel?.text = pictures.sorted()[indexPath.row]
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier:
+            "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            vc.pictureTotal = pictures.count
+            //vc.pictureIndex = selectedImage.indexPath.item
+            navigationController?.pushViewController(vc, animated: true) // Navigation controllers manage a stack of view controllers that can be pushed
+        }
+        /*
+         3 things in that one line have the potential to fail
+         if any of those things return nil, then the code inside the if let braces won’t run
+         */
     }
 }
