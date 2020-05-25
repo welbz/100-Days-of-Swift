4b14d3103d67537d9f44a443c3b3de239109f77f
Welby Jennings <hello@zakemedia.com.au>
2020-05-25 22:45:08 +1000

    Day-019
    Project 2
    - Setting up
    - Designing your layout

---
+++ Day-019-21/Project2/Project2/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Welby Jennings on 25/5/20.
+//  Copyright © 2020 Zake Media Pty Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
