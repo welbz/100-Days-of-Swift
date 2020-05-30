20dddb0b02c9581cca871631a1ddac2fe1b1b433
Welby Jennings <hello@zakemedia.com.au>
2020-05-30 20:14:18 +1000

    Day-025
    Project 4 - Part 2
    WKWebView, delegation, UIToolbar, UIProgressView, KVO

--- Day-024-26/Project 4/Project 4/ViewController.swift
+++ Day-024-26/Project 4/Project 4/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController, WKNavigationDelegate {
     // is UIViewController which extends UIViewController
     // conforms to WKNavigationDelegate protocol
     var webView: WKWebView!
+    var progressView: UIProgressView!
+    var websites = ["apple.com", "zakemedia.com.au"]
+    
+    
     
     override func loadView() {
         webView = WKWebView()
@@ -23,20 +27,36 @@ class ViewController: UIViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let url = URL(string: "https://zakemedia.com.au")!
-        // URL data type can be local files not websites
-        // Must be https
-        webView.load(URLRequest(url: url)) // wraps url in a URLRequest
-        webView.allowsBackForwardNavigationGestures = true // enables left and right swipe
+       navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
         
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil) // add left spacing to align refresh on right // eg Spacer()
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload)) // refresh button
+        
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit() // layout sizes to fit full progressView
+        let progressButton = UIBarButtonItem(customView: progressView) // wraps button as UIBarButtonItem in toolbar
+        
+        toolbarItems = [progressButton, spacer, refresh]
+        navigationController?.isToolbarHidden = false // toolbar will show
+        
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+        // Takes 4 Params
+        // who observer is, what property you want to observe, which value and context value
+        
+        let url = URL(string: "https://" + websites[0])!
+               // URL data type can be local files not websites
+               // Must be https
+               webView.load(URLRequest(url: url)) // wraps url in a URLRequest
+               webView.allowsBackForwardNavigationGestures = true // enables left and right swipe
     }
 
     @objc func openTapped() {
         let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
         
-        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
-        ac.addAction(UIAlertAction(title: "zakemedia.com.au", style: .default, handler: openPage))
+        for website in websites {
+            ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+        }
+        
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel)) // hides alert controller
         ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
         present(ac, animated: true)
@@ -50,6 +70,31 @@ class ViewController: UIViewController, WKNavigationDelegate {
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         title = webView.title
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" { // whether the keyPath parameter is set to estimatedProgress – that is, if the estimatedProgress value of the web view has changed
+            progressView.progress = Float(webView.estimatedProgress) // we set the progress property of our progress view to the new estimatedProgress value
+            // estimatedProgress is a Double so we need to convert as Float cause doubles hold more decimal places
+        }
+    }
+    
+    // WK Navigation Protocol
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url // let url be the url of the navigation to make code easier to read
+        
+        if let host = url?.host { // unwrap the optional url?.host //if the domain exists pull it out
+            for website in websites { // loop thorugh all websites in safe list
+                if host.contains(website) { // contains to check each safe website is in the host name
+                    decisionHandler(.allow) // if found, we want to allow loading
+                    return // safely return and exit method
+                }
+            }
+        }
+        
+        decisionHandler(.cancel)
+        // if the 'if let' fails or it succeeds but safe website does not exist in host
+        // contains is better than prefix as mobile sites url could be m.applle.com
     }
 }
 
