1062b6e4659917ce175d891544eb8fe8a952a4a1
Welby Jennings <hello@zakemedia.com.au>
2020-06-08 22:49:24 +1000

    Days-030-31
    Project 6 / Parts 1 & 2
    Advanced Auto Layout, Visual Formatting Language, Metrics, Priorities and Anchors

---
+++ Day-030-31/Project6a/Project2/ViewController.swift
@@ -0,0 +1,105 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Welby Jennings on 25/5/20.
+//  Copyright © 2020 Zake Media Pty Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries = [String]() //empty array
+    var score = 0
+    var correctAnswer = 0
+    var totalQuestions = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        button1.layer.borderWidth = 1 // 1 point which is 2 px on retina and 3px on HD devices
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        // see CALayer notes below
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        // UIColor has lightGray colour
+        
+        askQuestion()
+        
+    }
+    // CALayer sits beneath all your UIViews (that's the parent of UIButton, UITableView)
+    
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries.shuffle() // auto randomise array
+        correctAnswer = Int.random(in: 0...2)
+        
+        button1.setImage(UIImage(named: countries[0]),
+                         for: .normal) // standard state of a button
+        button2.setImage(UIImage(named: countries[1]),
+                         for: .normal)
+        button3.setImage(UIImage(named: countries[2]),
+                         for: .normal)
+        
+        title = countries[correctAnswer].uppercased() + " - Your Score: " + String(score)
+    }
+    
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        totalQuestions += 1
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong! That’s the flag of \(countries[sender.tag].uppercased())"
+            score -= 1
+        }
+        
+        if totalQuestions <= 10 {
+        let ac = UIAlertController(title: title, message: "Your current score is \(score)", preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "Continue",
+               style: .default, handler: askQuestion)) // dont use askQuestion()
+                // Alert options are .default, .cancel, and .destructive
+            present(ac, animated: true)
+        } else {
+            let ac = UIAlertController(title: title, message: "Your final score is \(score) / \(totalQuestions)", preferredStyle: .alert)
+            
+            ac.addAction(UIAlertAction(title: "Restart",
+                   style: .default, handler: askQuestion)) // dont use askQuestion()
+                    // Alert options are .default, .cancel, and .destructive
+                present(ac, animated: true)
+            
+            totalQuestions = 0
+            score = 0
+        }
+            
+    }
+    
+}
+
+/*
+ CALayer
+ By default, the border of CALayer is black, but you can change that if you want by using the UIColor data type. I said that CALayer brings with it a little more complexity, and here's where it starts to be visible:
+ 
+ CALayer sits at a lower technical level than UIButton, which means it doesn't understand what a UIColor is.
+ UIButton knows what a UIColor is because they are both at the same technical level, but CALayer is below UIButton, so UIColor is a mystery.
+ 
+ Don't despair, though: CALayer has its own way of setting colors called CGColor,
+ - which comes from Apple's Core Graphics framework.
+ - This, like CALayer, is at a lower level than UIButton, so the two can talk happily
+ 
+ Even better, UIColor (which sits above CGColor) is able to convert to and from CGColor easily, which means you don't need to worry about the complexity
+ */
---
+++ Day-030-31/Project6b/Project6b/ViewController.swift
@@ -0,0 +1,97 @@
+//
+//  ViewController.swift
+//  Project6b
+//
+//  Created by Welby Jennings on 8/6/20.
+//  Copyright © 2020 Zake Media Pty Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let label1 = UILabel()
+        label1.translatesAutoresizingMaskIntoConstraints = false // make constraints by hand
+        label1.backgroundColor = .red
+        label1.text = "THESE"
+        label1.sizeToFit()
+        
+        let label2 = UILabel()
+        label2.translatesAutoresizingMaskIntoConstraints = false
+        label2.backgroundColor = .cyan
+        label2.text = "ARE"
+        label2.sizeToFit()
+        
+        let label3 = UILabel()
+        label3.translatesAutoresizingMaskIntoConstraints = false
+        label3.backgroundColor = .yellow
+        label3.text = "SOME"
+        label3.sizeToFit()
+        
+        let label4 = UILabel()
+        label4.translatesAutoresizingMaskIntoConstraints = false
+        label4.backgroundColor = .green
+        label4.text = "AWESOME"
+        label4.sizeToFit()
+        
+        let label5 = UILabel()
+        label5.translatesAutoresizingMaskIntoConstraints = false
+        label5.backgroundColor = .orange
+        label5.text = "LABELS"
+        label5.sizeToFit()
+        
+        view.addSubview(label1)
+        view.addSubview(label2)
+        view.addSubview(label3)
+        view.addSubview(label4)
+        view.addSubview(label5)
+        
+//        let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
+//
+//        for label in viewsDictionary.keys {
+//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+//            // main view - assigns multiple constraints
+//            /*
+//             H:|[\(label)]| - VFL
+//             H = horizontal layout
+//             | symbol = edge of view controller
+//             ( labels ) puts label
+//             | symbol = edge of view controller
+//             */
+//        }
+//
+//        let metrics = ["labelHeight": 88]
+//
+//        // Vertical
+//        view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(==labelHeight@999)]-[label2(==label1)]-[label3(==label1)]-[label4(==label1)]-[label5(==label1)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
+        
+        /*
+            - means space and 10px by default
+         Priority 999
+         using "label1" for the height for other labels
+        */
+        
+        var previous: UILabel?
+        
+        // Anchors
+        for label in [label1, label2, label3, label4, label5] {
+            label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+            label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+            
+            if let previous = previous {
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+            } else {
+                label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true // pins to safe area - now displays below the notch
+            }
+            
+            previous = label // previous becomes the current one for next loop
+        }
+    
+    
+    }
+        
+}
+
