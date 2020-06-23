3d3dfe4f01466743c8bbbb13a1e1136eb39a041a
Welby Jennings <hello@zakemedia.com.au>
2020-06-23 22:36:08 +1000

    Day-046
    Project 11 / Part 2 - Complete

--- Day-045-47/Project11/Project11/GameScene.swift
+++ Day-045-47/Project11/Project11/GameScene.swift
@@ -10,6 +10,27 @@ import SpriteKit
 
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
+    var scoreLabel: SKLabelNode!
+    
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    
+    var editLabel: SKLabelNode!
+    
+    var editingMode: Bool = false {
+        didSet {
+            if editingMode {
+                editLabel.text = "Done"
+            } else {
+                editLabel.text = "Edit"
+            }
+        }
+    }
+    
+    
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "background")
         background.position = CGPoint(x: 512, y: 384)
@@ -17,6 +38,20 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         background.zPosition = -1
         addChild(background)
         
+        // Create scoreLabel and add to screen
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.text = "Score: 0"
+        scoreLabel.horizontalAlignmentMode = .right
+        scoreLabel.position = CGPoint(x: 980, y: 700)
+        addChild(scoreLabel)
+        
+        // Create editLabel and add to screen
+        editLabel = SKLabelNode(fontNamed: "Chalkduster")
+        editLabel.text = "Edit"
+        editLabel.position = CGPoint(x: 80, y: 700)
+        addChild(editLabel)
+        
+        // physics
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         physicsWorld.contactDelegate = self
         
@@ -31,28 +66,48 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         makeBouncer(at: CGPoint(x: 512, y: 0))
         makeBouncer(at: CGPoint(x: 768, y: 0))
         makeBouncer(at: CGPoint(x: 1024, y: 0))
-        
-        
-        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return } // first touches on screen
         let location = touch.location(in: self) // where touch happened in whole scene
+        let object = nodes(at: location)
         
-        let ball = SKSpriteNode(imageNamed: "ballRed")
-        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0) // behaves as balls not squares
-        ball.physicsBody?.restitution = 0.4 // bouncy-ness (optional)
+        if object.contains(editLabel) {
+            // editingMode = !editingMode - better to use toggle
+            editingMode.toggle()
+        } else {
+            if editingMode { // if in editing mode create box
+                // create box
+                let size = CGSize(width: Int.random(in: 16...128), height: 16)
+                let box = SKSpriteNode(color: UIColor(red: CGFloat.random(in: 0...1), green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha: 1), size: size)
+                // random red green blue
+                box.zRotation = CGFloat.random(in: 0...3)
+                box.position = location
+                
+                box.physicsBody = SKPhysicsBody(rectangleOf: box.size)
+                box.physicsBody?.isDynamic = false // dont allow it to move
+                addChild(box) // add tp game scene
+                
+            } else {
+                let ball = SKSpriteNode(imageNamed: "ballRed")
+                
+                ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0) // behaves as balls not squares
+                ball.physicsBody?.restitution = 0.4 // bouncy-ness (optional)
+                
+                // collisionBitMask - bounch off everything
+                // contactTestBitMask - tell us every bounch it has
+                ball.physicsBody?.contactTestBitMask = ball.physicsBody?.collisionBitMask
+                    ?? 0 // nil coalescing
+                
+                ball.position = location // sets postion at location of touch
+                ball.name = "ball"
+                addChild(ball) // calls in
+            }
+        }
         
-        // collisionBitMask - bounch off everything
-        // contactTestBitMask - tell us every bounch it has
-        ball.physicsBody?.contactTestBitMask = ball.physicsBody?.collisionBitMask
-            ?? 0 // nil coalescing
-        
-        ball.position = location // sets postion at location of touch
-        ball.name = "ball"
-        addChild(ball) // calls in
     }
+    
     
     // makeBouncer
     func makeBouncer(at position: CGPoint) {
@@ -97,8 +152,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     func collision(between ball: SKNode, object: SKNode) {
         if object.name == "good" {
             destroy(ball: ball)
+            score += 1
         } else if object.name == "bad" {
             destroy(ball: ball)
+            score -= 1
         }
     }
     
