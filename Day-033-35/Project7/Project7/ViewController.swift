77a6d4aee64f0e63c6401f1e2e2139c108914bab
Welby Jennings <hello@zakemedia.com.au>
2020-07-11 21:44:07 +1000

    Day-040 Refactoring
    Project 9
    - challenge 3 complete - Adding GCD

--- Day-033-35/Project7/Project7/ViewController.swift
+++ Day-033-35/Project7/Project7/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UITableViewController {
     
     var petitions = [Petition]()
-    var filteredPetitions = [Petition]() // Challenge 2
+    var filteredPetitions = [Petition]() // MARK: - Challenge 2
     let searchText = ""
     
     override func viewDidLoad() {
@@ -21,7 +21,7 @@ class ViewController: UITableViewController {
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(showFilters))
         
-        // Challenge 1
+        // MARK: - Challenge 1
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .bookmarks, target: self, action: #selector(showCredits))
         
         
@@ -45,7 +45,7 @@ class ViewController: UITableViewController {
         showError() // failed so show error
     }
     
-    // Challenge 1
+    // MARK: - Challenge 1
     @objc func showCredits() {
         let ac = UIAlertController(title: "Credits", message: "This data is from We The People API of the Whitehouse", preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
@@ -58,41 +58,45 @@ class ViewController: UITableViewController {
         
         let submitAction = UIAlertAction(title: "Search", style: .default) {
             // Closure
-            [weak self, weak ac] action in
+            [ weak self, weak ac ] action in
             guard let searchText = ac?.textFields?[0].text else { return }
-            self?.submit(searchText)
+            
+            //self?.submit(searchText)
+            
+            // MARK: - Project 9 - Challenge 3 - Background Thread
+            print("bg thread")
+            self?.performSelector(inBackground: #selector(self!.submit), with: searchText)
         }
         
         ac.addAction(submitAction)
         present(ac, animated: true)
-        
     }
     
-    // Challenge 2
-    func submit(_ searchText: String) {
+    // MARK: - Challenge 2
+    @objc func submit(_ searchText: String) {
+        let searchItem = searchText.lowercased()
+        print("Submitting \(searchItem)")
         
-        let searchItem: String = searchText.lowercased()
-        
-        print("Submitting")
-        print("Searched Word: \(searchItem)")
         print("Total Petitions: \(petitions.count)")
         print("Filtered Petitions: \(filteredPetitions.count)")
+        print("filtering for text \(searchItem)")
         
-        for item in petitions {
-            let itemTitle: String = item.title
-            let itemBody: String = item.body
-            
-            if itemTitle.contains("\(searchItem)") || itemBody.contains("\(searchItem)") {
-                filteredPetitions.insert(item, at: 0)
-                print("insering petitions containing \"\(searchItem)\" into filteredPetitions array")
-            }
+        // FIXME: - Not sure its filtering on both title and body
+        self.filteredPetitions = self.petitions.filter {
+                ($0.title).contains(searchItem) || ($0.body).contains(searchItem)
         }
         
         print("Petitions now in filteredPetitions: \(filteredPetitions.count)")
         
-        tableView.reloadData()
+        if filteredPetitions.isEmpty {
+            print("nothing found")
+        } else {
+            print("main thread - reloading tableView")
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
     }
-    
     
     func showError() {
         let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed try again", preferredStyle: .alert)
@@ -103,15 +107,18 @@ class ViewController: UITableViewController {
     func parse(json: Data) { // new parse method
         let decoder = JSONDecoder() // decoder
         
-        // asks decoder to convert its data to single a petitions object - find type and make instance of it from json
+        // Asks decoder to convert its data to single a petitions object - find type and make instance of it from json
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.results
-            tableView.reloadData()
+            
+            // MARK: - Project 9 - Challenge 3 - Main Thread - Is this the best location for main async
+            print("Starting main thread")
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
         }
         print("Total petitions loaded: \(petitions.count)")
     }
-    
-    
     
     // Table View Rows
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -138,4 +145,3 @@ class ViewController: UITableViewController {
     }
     
 }
-
