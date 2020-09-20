298a6827e65f60815681a2cb0d88025d98c9a45e
Welby Jennings <hello@zakemedia.com.au>
2020-09-20 23:51:22 +1000

    Day-085
    Project 26 / Part 1 / Video 2

--- Day-085-87 - Project 26/Project26/Project26/GameScene.swift
+++ Day-085-87 - Project 26/Project26/Project26/GameScene.swift
@@ -5,7 +5,7 @@
 //  Created by Welby Jennings on 20/9/20.
 //
 // https://www.hackingwithswift.com/100/85
-// Video 2  -https://www.hackingwithswift.com/read/26/2/loading-a-level-categorybitmask-collisionbitmask-contacttestbitmask
+// Video 2 -https://www.hackingwithswift.com/read/26/2/loading-a-level-categorybitmask-collisionbitmask-contacttestbitmask
 
 import SpriteKit
 
