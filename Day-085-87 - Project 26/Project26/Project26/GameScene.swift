0e48c504e25ee18411f37bc2280967ee0d02d0d9
Welby Jennings <hello@zakemedia.com.au>
2020-09-20 18:41:58 +1000

    Day-085
    Project 26 / Part 1 / Video 1

---
+++ Day-085-87 - Project 26/Project26/Project26/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  Project26
+//
+//  Created by Welby Jennings on 20/9/20.
+//
+// https://www.hackingwithswift.com/100/85
+// Video 2  -https://www.hackingwithswift.com/read/26/2/loading-a-level-categorybitmask-collisionbitmask-contacttestbitmask
+
+import SpriteKit
+
+class GameScene: SKScene {
+
+    override func didMove(to view: SKView) {
+        
+    }
+}
