37cd36aebc2f9b7793515c27ef7b3a1daf113ede
Welby Jennings <hello@zakemedia.com.au>
2020-09-20 13:00:40 +1000

    Day-083
    Project 25 / Part 1

---
+++ Day-083-84 - Project 25/Project25/Project25/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project25
+//
+//  Created by Welby Jennings on 20/9/20.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
